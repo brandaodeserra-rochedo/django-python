40ddca6bb7749a1f511e7456d5abf6f08054d1ba
Brandão <brandaodeserra@gmail.com>
2025-09-24 15:17:57 -0300

    atualização

--- cookbook/urls.py
+++ cookbook/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from cookbook.views import sobre_view, home_view, recipe_view
+from cookbook.views import sobre_view, home_view, recipe_view, category_view
 
 app_name = 'recipes'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('sobre/', sobre_view, name='sobre'),
     path('', home_view, name='home'),
     path('recipes/<int:id>/', recipe_view, name='recipe'),
+    path('recipes/category/<int:category_id>/', category_view, name="category")
 ]
--- cookbook/views.py
+++ cookbook/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from utils.recipes.factory import make_recipe
+from django.shortcuts import render, get_object_or_404,get_list_or_404
+from .models import Recipe
+from cookbook.models import Recipe
+from django.http import Http404
 
 
 # Create your videf sobre_view(request):
@@ -8,13 +10,31 @@ def sobre_view(request):
 
 
 def home_view(request):
+    recipes = Recipe.objects.filter(
+            is_published=True,
+        ).order_by('-id')
+    
     return render(request, 'cookbook/pages/home.html', context={
-        'recipes': [make_recipe() for _ in range(10)],
+        'recipes': recipes,
+    })
+
+
+def category_view(request, category_id):
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
+    return render(request, 'cookbook/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
 
 
 def recipe_view(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     return render(request, 'cookbook/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
