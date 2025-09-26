9f38789b95c766d4e1217d56507e9ad8f29615f8
Brandão <brandaodeserra@gmail.com>
2025-09-26 14:45:38 -0300

    Fim pagination

--- recipes/views.py
+++ recipes/views.py
@@ -1,21 +1,25 @@
-from django.shortcuts import render, get_object_or_404, get_list_or_404
-from django.http import Http404
-from recipes.models import Recipe
+import os
+
 from django.db.models import Q
+from django.http.response import Http404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from utils.pagination import make_pagination
 
+from recipes.models import Recipe
 
-# Create your videf sobre_view(request):
-def sobre_view(request):
-    return render(request, 'recipes/pages/sobre.html',
-                  context={'name': 'Brandão'})
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
 
 
 def home(request):
     recipes = Recipe.objects.filter(
-            is_published=True,
-        ).order_by('-id')
+        is_published=True,
+    ).order_by('-id')
+
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range
     })
 
 
@@ -26,14 +30,19 @@ def category(request, category_id):
             is_published=True,
         ).order_by('-id')
     )
+
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
         'title': f'{recipes[0].category.name} - Category | '
     })
 
 
 def recipe(request, id):
-    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
         'is_detail_page': True,
@@ -51,11 +60,21 @@ def search(request):
             Q(title__icontains=search_term) |
             Q(description__icontains=search_term),
         ),
-        Q(is_published=True)
+        is_published=True
     ).order_by('-id')
+
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}',
     })
+
+
+# Create your videf sobre_view(request):
+def sobre_view(request):
+    return render(request, 'recipes/pages/sobre.html',
+                  context={'name': 'Brandão'})
