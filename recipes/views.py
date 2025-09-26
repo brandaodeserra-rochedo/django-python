16b3cca6dc2180677e860908871a6cf60a92f624
Brandão <brandaodeserra@gmail.com>
2025-09-26 10:18:26 -0300

    busca feature

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import Http404
 from recipes.models import Recipe
+from django.db.models import Q
 
 
 # Create your videf sobre_view(request):
@@ -44,8 +45,14 @@ def search(request):
 
     if not search_term:
         raise Http404()
+    
+    recipes = Recipe.objects.filter(
+        Q(is_published=True),
+        Q(title__icontains=search_term) | Q(description__icontains=search_term)
+    ).order_by('-id')
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
+        'recipes': recipes,
     })
