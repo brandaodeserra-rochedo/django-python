82745f280b7bd0c2573328d24eb0247415ba6816
Brandão <brandaodeserra@gmail.com>
2025-09-28 17:06:02 -0300

    testes authors

--- authors/views.py
+++ authors/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.http import Http404
 from django.shortcuts import redirect, render
 
@@ -18,6 +19,14 @@ def register_create(request):
 
     POST = request.POST
     request.session['register_form_data'] = POST
-    RegisterForm(POST)
+    form = RegisterForm(POST)
+
+    if form.is_valid():
+        user = form.save(commit=False)
+        user.set_password(user.password)
+        user.save()
+        messages.success(request, 'Your user is created, please log in.')
+
+        del (request.session['register_form_data'])
 
     return redirect('authors:register')
