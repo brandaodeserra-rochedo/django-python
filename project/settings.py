af5aa8fe4df5071abbc8965df48a0d8b3fcbb6e0
Brandão <brandaodeserra@gmail.com>
2025-10-14 09:57:28 -0300

    CORS

--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'rest_framework',
     # nossos apps
     'recipes',
@@ -51,10 +52,16 @@ INSTALLED_APPS = [
     'debug_toolbar',
 ]
 
+# Liberando Dominios
+CORS_ALLOWED_ORIGINS = parse_comma_sep_str_to_list(
+    get_env_variable('CORS_ALLOWED_ORIGINS')
+)
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
