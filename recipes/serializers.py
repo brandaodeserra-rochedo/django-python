f8a705374b7288450dfe21f7e9209e90908d7fe9
Brandão <brandaodeserra@gmail.com>
2025-10-14 09:29:22 -0300

    Fim Rest

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,7 +1,8 @@
+from authors.validators import AuthorRecipeValidator
 from rest_framework import serializers
 from tag.models import Tag
 
-from .models import Recipe
+from recipes.models import Recipe
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -17,6 +18,9 @@ class RecipeSerializer(serializers.ModelSerializer):
             'id', 'title', 'description', 'author',
             'category', 'tags', 'public', 'preparation',
             'tag_objects', 'tag_links',
+            'preparation_time', 'preparation_time_unit', 'servings',
+            'servings_unit',
+            'preparation_steps', 'cover'
         ]
 
     public = serializers.BooleanField(
@@ -43,3 +47,26 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     def any_method_name(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
+
+    def validate(self, attrs):
+        if self.instance is not None and attrs.get('servings') is None:
+            attrs['servings'] = self.instance.servings
+
+        if self.instance is not None and attrs.get('preparation_time') is None:
+            attrs['preparation_time'] = self.instance.preparation_time
+
+        super_validate = super().validate(attrs)
+        AuthorRecipeValidator(
+            data=attrs,
+            ErrorClass=serializers.ValidationError,
+        )
+        return super_validate
+
+    def save(self, **kwargs):
+        return super().save(**kwargs)
+
+    def create(self, validated_data):
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        return super().update(instance, validated_data)
