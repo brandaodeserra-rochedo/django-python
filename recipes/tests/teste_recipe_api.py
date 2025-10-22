eff6f51618d3b025a7f2f850fbb8c71fafb279f0
Brandão <brandaodeserra@gmail.com>
2025-10-22 13:52:40 -0300

    teste API 2

--- recipes/tests/teste_recipe_api.py
+++
@@ -1,7 +0,0 @@
-from recipes.tests.test_recipe_base import RecipeMixin
-from rest_framework import test
-
-
-class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
-    def test_the_test(self):
-        assert 1 == 1
